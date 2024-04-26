acfbb908f8cd171971e8163c97016af06e169d84
mounikasomala <121745911+mounikasomala@users.noreply.github.com>
2024-04-26 22:55:02 +0530

    Create hackerrank task1 loops.py

---
+++ hackerrank task1 loops.py
@@ -0,0 +1,14 @@
+# The provided code stub reads and integer,n , from STDIN. For all non-negative integers i<n, print i^2.
+if __name__ == '__main__':
+    n = int(input().strip())
+    for i in range(n):
+       print(i**2)
+      
+# input(stdin)
+# 5
+# output
+# 0
+# 1
+# 4
+# 9
+# 16
