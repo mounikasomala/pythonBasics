f70c259526d833224a0a0c308e4e7a0ebf0cf51e
mounikasomala <121745911+mounikasomala@users.noreply.github.com>
2024-04-28 11:37:23 +0530

    Create List-2.py

---
+++ List-2.py
@@ -0,0 +1,17 @@
+g=[111,12,13,43,-12,33,54,12]
+n=len(g)
+target =54
+for i in range(n):
+    print(g[i])
+    if g[i]==target:
+        print("found")
+# Output
+# 111
+# 12
+# 13
+# 43
+# -12
+# 33
+# 54
+# found
+# 12
